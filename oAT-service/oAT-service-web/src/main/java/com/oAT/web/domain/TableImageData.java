bb06e53fe9d3b93ba201940dbb90955d0d9a8cad
xiaoxiao <39261902+000xiaoxiao000@users.noreply.github.com>
2026-05-07 17:58:35 +0800

    优化

--- oAT-service/oAT-service-web/src/main/java/com/oAT/web/domain/TableImageData.java
+++ oAT-service/oAT-service-web/src/main/java/com/oAT/web/domain/TableImageData.java
@@ -4,7 +4,6 @@ public class TableImageData extends ImageData{
 
     public String dataBaseType;
     public String database;
-    public String[] sqlContents;
 
     public TableImageData(String id) {
         super(id);
