7a2a44cd1fbfc38bd3b376bed8a8c916462ce199
xiaoxiao <570934591@qq.com>
2026-06-24 14:26:45 +0800

    优化

--- oAT-relay/src/main/java/com/oAT/relay/OAtRelayApplication.java
+++ oAT-relay/src/main/java/com/oAT/relay/OAtRelayApplication.java
@@ -1,5 +1,6 @@
 package com.oAT.relay;
 
+import com.oAT.relay.config.RelayProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
